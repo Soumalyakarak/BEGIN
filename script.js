4679673c703959cba56dd0b5e624f6fb986451fe
Soumalya Karak <soumalyakarak2@gmail.com>
2025-02-08 14:32:42 +0530

    2nd file canges

---
+++ script.js
@@ -0,0 +1,12 @@
+function greet(name) {
+    return "Hello, " + name + "!";
+  }
+  
+  const message = greet("World");
+  console.log(message);
+  
+  const numbers = [1, 2, 3, 4, 5];
+  
+  const doubledNumbers = numbers.map(number => number * 2);
+  
+  console.log(doubledNumbers);
